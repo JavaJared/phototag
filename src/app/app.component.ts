fe591a4f55b8ecbe40fffb7a4c2eb1a75fdd5cad
JavaJared <jaredellis533@gmail.com>
2024-02-28 18:38:24 -0500

    Built out functionality of app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,15 +14,17 @@ import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutlin
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    {
+      title: 'Take a photo',
+      url: '/capture',
+      icon: 'camera'
+    },
+    {
+      title: 'View gallery',
+      url: '/view',
+      icon: 'globe'
+    }
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {
-    addIcons({ mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp });
-  }
+  constructor() {}
 }
+
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
@@ -6,6 +6,9 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage'
 
 if (environment.production) {
   enableProdMode();
@@ -15,6 +18,9 @@ bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes),
+    provideRouter(routes), 
+    importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"phototag-dcf53","appId":"1:1088104973620:web:0973f36a7fab10c9739161","storageBucket":"phototag-dcf53.appspot.com","apiKey":"AIzaSyBSE5yX4DXyX2U7Fnz1DuahmAczQvFFcLk","authDomain":"phototag-dcf53.firebaseapp.com","messagingSenderId":"1088104973620"}))),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideStorage(() => getStorage()))
   ],
 });
